74dea298a935e2956f8eec518c1b907ef65e8493
John C. Daub <hsoi@hsoi.com>
2015-05-08 06:48:08 -0500

    Initial swift (re)implementation.

---
+++ HEAlert/HEAlert.swift
@@ -0,0 +1,120 @@
+//
+//  HEAlert.swift
+//
+//  Created by hsoi on 2010-12-24, converted to Swift 2015-05-08
+//
+//  HEAnalytics - Copyright (c) 2010, 2015, Hsoi Enterprises LLC
+//  All rights reserved.
+//  hsoi@hsoienterprises.com
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//  * Neither the name of HEAnalytics nor the names of its
+//  contributors may be used to endorse or promote products derived from
+//  this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import Foundation
+import UIKit
+
+
+@objc public class HEAlert {
+    
+    class func ok(title: String) {
+        HEAlert.showAlert(title, message: nil, buttonTitle: nil, presentingViewController: nil)
+    }
+    
+    
+    
+    class func ok(title: String, presentingViewController: UIViewController) {
+        HEAlert.showAlert(title, message: nil, buttonTitle: nil, presentingViewController: presentingViewController)
+    }
+    
+    class func ok(title: String, message: String, presentingViewController: UIViewController) {
+        HEAlert.showAlert(title, message: message, buttonTitle: nil, presentingViewController: presentingViewController)
+    }
+    
+    
+    
+    class func other(title: String, buttonTitle: String, presentingViewController: UIViewController) {
+        HEAlert.showAlert(title, message: nil, buttonTitle: buttonTitle, presentingViewController: presentingViewController)
+    }
+    
+    class func other(title: String, message: String, buttonTitle: String, presentingViewController: UIViewController) {
+        HEAlert.showAlert(title, message: message, buttonTitle: buttonTitle, presentingViewController: presentingViewController)
+    }
+    
+    
+    
+    class func error(error: NSError, presentingViewController: UIViewController?) {
+        HEAlert.showAlert(error.localizedDescription, message: error.localizedRecoverySuggestion, buttonTitle: nil, presentingViewController: presentingViewController)
+    }
+    
+    
+    
+    private class func showAlert(title: String, message: String?, buttonTitle: String?, presentingViewController: UIViewController?) {
+
+        // Hsoi 2015-05-08 - One useful thing about UIAlertView is you can present them from anywhere -- you don't
+        // need a presenting UIViewController. While some may argue you should never show an alert from
+        // a place where you don't have access to a UIViewController (e.g. from within model code), sometimes
+        // reality or necessity trumps ideals and you need this ability.
+        //
+        // This is why the presentingViewController is optional. If you want you CAN pass nil, meaning
+        // we don't have a UIViewController, but we still want to present an alert. I chose to NOT make
+        // it have a default value of nil tho, so it's clear in code this is an explicit choice
+        // and not just something we perhaps forgot to type.
+        //
+        // For now, if presentingViewController is nil, we will fall back to UIAlertView. I filed
+        // rdar://problem/18297508 back in 2014-09-10 requesting some way to still be able to present
+        // alerts without need for a presentingViewController, but as of this writing there's been no
+        // comment or movement on it (and honestly I'm not expecting Apple will do anything about this).
+        // One alternative implementation is to get the window's rootViewController, but I don't think
+        // that's ideal (what if you have more than 1 window?)
+        //
+        // Ideally, you should pass a presentingViewController -- and this is why most of the HEAlert API makes
+        // the presentingViewController non-optional. However, we contend for it, and just realize that if you
+        // do use a nil presentingViewController that it may have issues, may be less than ideal, and may have to
+        // go away someday and you'll then have to figure out how to (re)solve the issue into having a
+        // UIViewController to present from -- thus probably best to just solve that problem now.
+
+        var okTitle: String = NSLocalizedString("OK", comment: "Alert button title")
+        if let buttonTitle = buttonTitle where !buttonTitle.isEmpty {
+            okTitle = buttonTitle
+        }
+        
+        
+        if let presentingViewController = presentingViewController {
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+            
+            let okAction = UIAlertAction(title: okTitle, style: .Default, handler: nil)
+            alertController.addAction(okAction)
+            
+            presentingViewController.presentViewController(alertController, animated: true, completion: nil)
+        }
+        else {
+            let alertView = UIAlertView(title: title, message: message, delegate: nil, cancelButtonTitle: okTitle)
+            alertView.show()
+        }
+    }
+ 
+    
+
+}
